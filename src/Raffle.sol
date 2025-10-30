dc7c567fb887e1564857570f205154f4554d3960
Ebin234 <ebinyesudas234@gmail.com>
2025-10-30 10:40:04 +0000

    first commit

---
+++ src/Raffle.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+/**
+ * @title Raffle Contract
+ * @author Ebin Yesudas
+ * @notice This contract is for creating Lotteries.
+ * @dev Implements chainlink VRF
+ */
+
+contract Raffle{}
